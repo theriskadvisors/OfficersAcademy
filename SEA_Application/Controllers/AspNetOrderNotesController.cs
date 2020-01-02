ff9ac460b1be64002e8ad34ed530715764a25566
shahzadqasir2501 <59007872+shahzadqasir2501@users.noreply.github.com>
2020-01-02 13:57:23 +0500

    Order V2

--- SEA_Application/Controllers/AspNetOrderNotesController.cs
+++ SEA_Application/Controllers/AspNetOrderNotesController.cs
@@ -6,22 +6,24 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using Microsoft.AspNet.Identity;
 using SEA_Application.Models;
 
 namespace SEA_Application.Controllers
 {
-    public class AspNetNotesController : Controller
+    public class AspNetOrderNotesController : Controller
     {
         private SEA_DatabaseEntities db = new SEA_DatabaseEntities();
 
-        // GET: AspNetNotes
+        // GET: AspNetOrderNotes
         public ActionResult Index()
         {
             var aspNetNotes = db.AspNetNotes.Include(a => a.AspNetSubject);
             return View(aspNetNotes.ToList());
         }
 
-        // GET: AspNetNotes/Details/5
+        // GET: AspNetOrderNotes/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -35,24 +37,86 @@ namespace SEA_Application.Controllers
             }
             return View(aspNetNote);
         }
+        public ActionResult RecentOrders()
+        {
+            var CurrentUserId = User.Identity.GetUserId();
 
-        // GET: AspNetNotes/Create
+            int StudentId = db.AspNetStudents.Where(x => x.StudentID == CurrentUserId).FirstOrDefault().Id;
+
+            var result = from Notes in db.AspNetNotes
+                         join OrderNotes in db.AspNetNotesOrders on Notes.Id equals OrderNotes.NotesID
+                         where OrderNotes.StudentID == StudentId
+                         select new
+                         {
+                             Id = OrderNotes.Id,
+                             Title = Notes.Title,
+                             Discription = Notes.Description,
+                             CourseType = Notes.CourseType,
+                             Price = Notes.Price,
+                             Quantity = OrderNotes.Quantity,
+                             Status = OrderNotes.Status,
+
+                         };
+                
+      
+
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult CancelOrder(int OrderId)
+        {
+
+          AspNetNotesOrder NotesOrder=   db.AspNetNotesOrders.Where(x => x.Id == OrderId).FirstOrDefault();
+            NotesOrder.Status = "Cancelled";
+
+            db.Entry(NotesOrder).State = EntityState.Modified;
+
+            db.SaveChanges();
+
+            return Json("", JsonRequestBehavior.AllowGet);
+        }
+
+
+
+            public ActionResult ConfirmOrder(int NotesId, int Quantity)
+        {
+
+            var CurrentUserId = User.Identity.GetUserId();
+
+            int StudentId = db.AspNetStudents.Where(x => x.StudentID == CurrentUserId).FirstOrDefault().Id;
+
+
+            AspNetNotesOrder NotesOrder = new AspNetNotesOrder();
+
+            NotesOrder.NotesID = NotesId;
+            NotesOrder.StudentID = StudentId;
+
+            NotesOrder.Quantity = Quantity;
+            NotesOrder.Status = "Draft";
+
+            db.AspNetNotesOrders.Add(NotesOrder);
+
+
+            db.SaveChanges();
+
+            return Json("", JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: AspNetOrderNotes/Create
         public ActionResult Create()
         {
             ViewBag.SubjectID = new SelectList(db.AspNetSubjects, "Id", "SubjectName");
             return View();
         }
 
-        // POST: AspNetNotes/Create
+        // POST: AspNetOrderNotes/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Title,Description,SubjectID,CourseType,Price,CreationDate")] AspNetNote aspNetNote)
         {
-
-            aspNetNote.CourseType =  Request.Form["CourseType"];
-            aspNetNote.CreationDate = DateTime.Now;
             if (ModelState.IsValid)
             {
                 db.AspNetNotes.Add(aspNetNote);
@@ -64,7 +128,7 @@ namespace SEA_Application.Controllers
             return View(aspNetNote);
         }
 
-        // GET: AspNetNotes/Edit/5
+        // GET: AspNetOrderNotes/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -80,9 +144,9 @@ namespace SEA_Application.Controllers
             return View(aspNetNote);
         }
 
-        // POST: AspNetNotes/Edit/5
+        // POST: AspNetOrderNotes/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Title,Description,SubjectID,CourseType,Price,CreationDate")] AspNetNote aspNetNote)
@@ -97,7 +161,7 @@ namespace SEA_Application.Controllers
             return View(aspNetNote);
         }
 
-        // GET: AspNetNotes/Delete/5
+        // GET: AspNetOrderNotes/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -112,7 +176,7 @@ namespace SEA_Application.Controllers
             return View(aspNetNote);
         }
 
-        // POST: AspNetNotes/Delete/5
+        // POST: AspNetOrderNotes/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
