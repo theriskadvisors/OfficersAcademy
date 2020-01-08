a354ea1caebc23f205cb66e1dfa4c0b3a4c74363
shahzadqasir2501 <59007872+shahzadqasir2501@users.noreply.github.com>
2020-01-08 18:13:27 +0500

    studentfeedetails1

---
+++ SEA_Application/Models/StdFeeDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SEA_Application.Models
+{
+    public class StdFeeDetail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string UserName { get; set; }
+
+        public double? InstalmentAmount { get; set; }
+
+        public string Months { get; set; }
+
+        public string Status { get; set; }
+
+        public string ClassName { get; set; }
+        public string CourseType { get; set; }
+
+    }
+}
