d1e1bf791bc4d63c9e958d72f8b0bad976b3300f
shahzadqasir2501 <59007872+shahzadqasir2501@users.noreply.github.com>
2020-03-11 11:36:06 +0500

    GetLocalDateTimeClass

---
+++ SEA_Application/Models/GetLocalDateTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SEA_Application.Models
+{
+    public static class GetLocalDateTime
+    {
+
+        public static DateTime GetLocalDateTimeFunction()
+        {
+            TimeZone time2 = TimeZone.CurrentTimeZone;
+            DateTime test = time2.ToUniversalTime(DateTime.Now);
+            var pakistan = TimeZoneInfo.FindSystemTimeZoneById("Pakistan Standard Time");
+            DateTime pakistantime = TimeZoneInfo.ConvertTimeFromUtc(test, pakistan);
+            return pakistantime;
+
+        }
+
+    }
+}
