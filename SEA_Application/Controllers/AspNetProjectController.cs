ec0bec79aac45aa7ef5e5295741d7ded22589f8b
theriskadvisors <38946878+theriskadvisors@users.noreply.github.com>
2020-04-07 15:55:38 +0500

    tpoibcs

--- SEA_Application/Controllers/AspNetProjectController.cs
+++ SEA_Application/Controllers/AspNetProjectController.cs
@@ -35,6 +35,17 @@ namespace SEA_Application.Controllers
                return Content(status);
 
         }
+      
+             public ActionResult GetLession(int TopID)
+        {
+            var TopicList = db.AspnetLessons.Where(x => x.TopicId == TopID ).ToList().Select(x => new { x.Id, x.Name });
+
+            string status = Newtonsoft.Json.JsonConvert.SerializeObject(TopicList);
+
+            // return Json(SubjectsByClass, JsonRequestBehavior.AllowGet);
+            return Content(status);
+
+        }
         public ActionResult GetTopic(int SubID)
         {
             var TopicList = db.AspnetSubjectTopics.Where(x => x.SubjectId == SubID ).ToList().Select(x => new { x.Id, x.Name });
